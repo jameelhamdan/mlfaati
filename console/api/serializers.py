2879a124e90d6eabd93713c73d11e81a781737cf
Kingjmk <jameelhamdan99@yahoo.com>
2021-01-13 16:17:39 +0200

    Added file browser vue app

--- console/api/serializers.py
+++ console/api/serializers.py
@@ -11,10 +11,11 @@ class FileSerializer(serializers.ModelSerializer):
 
 class FolderSerializer(serializers.ModelSerializer):
     url = serializers.SerializerMethodField()
+    content_length = serializers.IntegerField(source='files_total_size', default=0)
 
     def get_url(self, obj):
         return reverse('console:api_browser_folder', kwargs={'pk': obj.space_id, 'folder_id': obj.pk})
 
     class Meta:
         model = core.models.Folder
-        fields = ['id', 'name', 'path', 'url', 'full_path', 'created_on', 'updated_on']
+        fields = ['id', 'name', 'path', 'url', 'content_length', 'full_path', 'created_on', 'updated_on']
