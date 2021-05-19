d051a3c5cc7cb3659cb8e4d25f8c8716037bd192
Kingjmk <jameelhamdan99@yahoo.com>
2021-05-19 09:33:38 +0300

    quick fix

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-$./scripts/download_models.sh
-$./scripts/build_docs.sh
+./scripts/download_models.sh
+./scripts/build_docs.sh
 python -m pip install -r requirements-extra.txt --no-cache-dir
 python manage.py collectstatic --no-input
 python manage.py migrate
