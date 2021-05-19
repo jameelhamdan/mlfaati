d93a4165354896b62edaf46b98e41ed6c42cff41
Kingjmk <jameelhamdan99@yahoo.com>
2021-05-19 09:30:27 +0300

    Some changed to commandline

--- scripts/download_models.sh
+++ scripts/download_models.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Downloading models..."
-curl -LJ https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/inception_v3_weights_tf_dim_ordering_tf_kernels.h5 --output bin/models/inception_v3_weights_tf_dim_ordering_tf_kernels.h5
+mkdir -p ./bin/models/
+curl -LJ https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/inception_v3_weights_tf_dim_ordering_tf_kernels.h5 --output ./bin/models/inception_v3_weights_tf_dim_ordering_tf_kernels.h5
 echo "Finished downloading"
 exec "$@"
--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-./scripts/download_models.sh
-./scripts/build_docs.sh
-python -m pip install -r requirements-extra.txt
+$./scripts/download_models.sh
+$./scripts/build_docs.sh
+python -m pip install -r requirements-extra.txt --no-cache-dir
 python manage.py collectstatic --no-input
 python manage.py migrate
 exec "$@"
